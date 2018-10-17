27cb24256b2708046d3b6617552c1653806bd9f4
Lauren Piontek <piontekle@gmail.com>
2018-10-17 18:56:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,26 +1,33 @@
 const app = "I don't do much."
-var dodger = document.getElementById('dodger')
+
+var dodger = document.getElementById('dodger');
 
 function moveDodgerLeft() {
-var leftNumbers = dodger.style.left.replace('px', '')
-var left = parseInt(leftNumbers, 10)
+var leftNumbers = dodger.style.left.replace('px', '');
+var left = parseInt(leftNumbers, 10);
 
 if (left > 0) {
-dodger.style.left = `${left - 1}px`
+dodger.style.left = `${left - 1}px`;
 }
 }
 
 document.addEventListener('keydown', function(e) {
 if (e.which === 37) {
-moveDodgerLeft()
+moveDodgerLeft();
 }
-})
+});
 
 function moveDodgerRight() {
-var rightNumbers = dodger.style.right.replace('px', '')
-var right = parseInt(rightNumbers, 10)
+var rightNumbers = dodger.style.right.replace('px', '');
+var right = parseInt(rightNumbers, 10);
 
 if (right > 0) {
-dodger.style.right = `${right - 1}px`
+dodger.style.right = `${right - 1}px`;
 }
 }
+
+document.addEventListener('keydown', function(e) {
+if (e.which === 39) {
+moveDodgerRight();
+}
+});
